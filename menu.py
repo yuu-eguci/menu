5e6be0e1f78f3f88f9aec2b1cc24d15ffc3fd10d
yuu-eguci <yuu.eguci@gmail.com>
2020-01-31 08:47:41 +0900

    不要なものは書かない。記録は git の履歴に残せば OK だよ。

--- menu.py
+++ menu.py
@@ -14,7 +14,6 @@ class app_menu(tk.Frame):
         self.master.geometry('500x500')
 
         self.menuinput()
-        # self.setvar()     # buttoncommandに組み込んだから不要。
         self.create_widgets()
 
     # ウィジェットを作っていくよ。
@@ -61,20 +60,10 @@ class app_menu(tk.Frame):
             self.menutext_3.set('')
             self.menutext_4.set('そんなに作れないからだめ！')
 
-        # self.menutext_1.set(self.menulist[self.num][0])
-        # self.menutext_2.set(self.menulist[self.num][1])
-        # self.menutext_3.set(self.menulist[self.num][2])
-
     # csvファイル読み込んでリストにするよ。
     def menuinput(self):
         with open('menu.csv', encoding='utf-8') as f:
             self.menulist = list(csv.reader(f))
-        # print(self.menulist)
-
-    # ランダムな数字つくるよ。
-    # 何回も変更したいから、buttonコマンドにくみこんだよ。
-    # def setvar(self):
-    #     self.num = rd.randint(0, len(self.menulist)-1)
 
 
 root = tk.Tk()
